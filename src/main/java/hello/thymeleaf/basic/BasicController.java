7a1eb4a8b1eef332235db7b95c74f1827f82ed2a
BaekEuiHun <euihunbaek@gmail.com>
2024-09-12 22:23:13 +0900

    each 반복문

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -54,17 +54,33 @@ public class BasicController {
     session.setAttribute("sessionData", "Hello Session");
     return "basic/basic-objects";
   }
+
   @GetMapping("/operation")
   public String operation(Model model) {
     model.addAttribute("nullData", null);
     model.addAttribute("data", "Spring!");
     return "basic/operation";
   }
+
   @GetMapping("/link")
   public String link(Model model) {
     model.addAttribute("param1", "data1");
     model.addAttribute("param2", "data2");
     return "basic/link";
+  }
+
+  @GetMapping("/each")
+  public String each(Model model) {
+    addUsers(model);
+    return "basic/each";
+  }
+
+  private void addUsers(Model model) {
+    List<User> list = new ArrayList<>();
+    list.add(new User("userA", 20));
+    list.add(new User("userB", 40));
+    list.add(new User("userC", 30));
+    model.addAttribute("users", list);
   }
 
   @Component("helloBean")
